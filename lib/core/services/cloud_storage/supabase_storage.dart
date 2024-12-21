25ed75558e2070337d551c7c673207622f87dd0f
ISAM ELZOBI <alzeabiesam@gmail.com>
2024-12-21 23:04:43 +0300

    Refactor SupabaseStorage to SupabaseStorageService and initialize in main
    Refactor the SupabaseStorage class to SupabaseStorageService and add an initialization method. Update main.dart to initialize SupabaseStorageService before running the app. This ensures that Supabase is properly initialized before any storage operations are attempted.

--- lib/core/services/cloud_storage/supabase_storage.dart
+++ lib/core/services/cloud_storage/supabase_storage.dart
@@ -1,12 +1,21 @@
 import 'dart:io';
 
 import 'package:fruits_hub_admin/core/services/cloud_storage/cloud_storage.dart';
+import 'package:fruits_hub_admin/core/utils/keys.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class SupabaseStorage implements CloudStorage {
+class SupabaseStorageService implements CloudStorage {
+  static late Supabase _supabase;
+
+  static Future<void> initialize() async {
+    _supabase = await Supabase.initialize(
+      url: KSupabaseUrl,
+      anonKey: KSupabaseKey,
+    );
+  }
+
   @override
   Future<String> uploadFile(File file, String path) {
     throw UnimplementedError();
-    
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,29 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub_admin/core/helper_functions/on_generate_routes.dart';
+import 'package:fruits_hub_admin/core/services/cloud_storage/supabase_storage.dart';
 import 'package:fruits_hub_admin/core/services/custom_bloc_observer.dart';
 import 'package:fruits_hub_admin/core/services/get_it_service.dart';
-import 'package:fruits_hub_admin/core/utils/keys.dart';
 import 'package:fruits_hub_admin/features/dashboard/views/dashboard_view.dart';
 import 'package:fruits_hub_admin/firebase_options.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  // Ensure widgets are initialized
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Supabase.initialize(
-    url: KSupabaseUrl,
-    anonKey: KSupabaseKey,
-  );
+  // Initialize Supabase
+  await SupabaseStorageService.initialize();
 
+  // Bloc Observer
   Bloc.observer = CustomBlocObserver();
 
+  // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  // Initialize GetIt
   setupGetIt();
+
+  // Run the app
   runApp(const MyApp());
 }
 
