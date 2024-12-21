30a678da08efeaa86eafa70c77f36d45b324c0e8
ISAM ELZOBI <mobarmeg.9@gmail.com>
2024-12-21 23:23:26 +0300

    Implement Supabase file upload and switch to SupabaseStorageService
    Updated the SupabaseStorageService to implement the uploadFile method, extracting the file name and extension for proper storage. Also, modified the get_it_service.dart to use SupabaseStorageService instead of FirebaseCloudStorage.

--- lib/core/services/cloud_storage/supabase_storage.dart
+++ lib/core/services/cloud_storage/supabase_storage.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 import 'package:fruits_hub_admin/core/services/cloud_storage/cloud_storage.dart';
 import 'package:fruits_hub_admin/core/utils/keys.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:path/path.dart' as p;
 
 class SupabaseStorageService implements CloudStorage {
   static late Supabase _supabase;
 
   static Future<void> initialize() async {
-
     _supabase = await Supabase.initialize(
       url: KSupabaseUrl,
       anonKey: KSupabaseKey,
@@ -16,8 +16,13 @@ class SupabaseStorageService implements CloudStorage {
   }
 
   @override
-  Future<String> uploadFile(File file, String path) {
-    throw UnimplementedError();
-    
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = p.basename(file.path);
+    String extensionName = p.extension(fileName);
+    var result = await _supabase.client.storage
+        .from('fruits_images')
+        .upload('$path/$fileName.$extensionName', file);
+
+    return result;
   }
 }
--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -3,7 +3,7 @@ import 'package:fruits_hub_admin/core/repos/images_repo/images_repo_impl.dart';
 import 'package:fruits_hub_admin/core/repos/product_repo/products_repo.dart';
 import 'package:fruits_hub_admin/core/repos/product_repo/products_repo_impl.dart';
 import 'package:fruits_hub_admin/core/services/cloud_storage/cloud_storage.dart';
-import 'package:fruits_hub_admin/core/services/cloud_storage/firebase_cloud_storage.dart';
+import 'package:fruits_hub_admin/core/services/cloud_storage/supabase_storage.dart';
 import 'package:fruits_hub_admin/core/services/data_service.dart';
 import 'package:fruits_hub_admin/core/services/firestore_service.dart';
 import 'package:get_it/get_it.dart';
@@ -11,7 +11,12 @@ import 'package:get_it/get_it.dart';
 final getIt = GetIt.instance;
 
 void setupGetIt() {
-  getIt.registerSingleton<CloudStorage>(FirebaseCloudStorage());
+  // Cloud Storage Firebase
+  //getIt.registerSingleton<CloudStorage>(FirebaseCloudStorage());
+
+  // Cloud Storage Supabase
+  getIt.registerSingleton<CloudStorage>(SupabaseStorageService());
+
   getIt.registerSingleton<DatabaseService>(FireStoreService());
 
   getIt.registerSingleton<ImagesRepo>(
