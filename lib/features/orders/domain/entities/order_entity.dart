ff407ca6bd88223abc26e8a3a23ac85e2893d01c
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-05-28 23:02:42 +0300

    update GeneratedPluginRegistrant.swift with sqflite_darwin

--- lib/features/orders/domain/entities/order_entity.dart
+++ lib/features/orders/domain/entities/order_entity.dart
@@ -1,12 +1,16 @@
+import 'package:fruits_hub_admin/core/enums/order_enum.dart';
 import 'package:fruits_hub_admin/features/orders/domain/entities/order_product_entity.dart';
 import 'package:fruits_hub_admin/features/orders/domain/entities/shipping_address_entity.dart';
 
 class OrderEntity {
   final num totalPrice;
   final String uId;
+  final String orderID;
   final ShippingAddressEntity shippingAddressEntity;
   final List<OrderProductEntity> orderProducts;
   final String paymentMethod;
+  final OrderStatusEnum status;
+
 
   OrderEntity({
     required this.totalPrice,
@@ -14,15 +18,9 @@ class OrderEntity {
     required this.shippingAddressEntity,
     required this.orderProducts,
     required this.paymentMethod,
+    required this.status,
+    required this.orderID,
   });
 
-  toEntity() {
-    return OrderEntity(
-      totalPrice: totalPrice,
-      uId: uId,
-      shippingAddressEntity: shippingAddressEntity.toEntity(),
-      orderProducts: orderProducts.map((e) => e.toEntity()).toList(),
-      paymentMethod: paymentMethod,
-    );
-  }
+
 }
