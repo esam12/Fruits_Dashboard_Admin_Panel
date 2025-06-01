9173f12be366bb2e8f7372fff5ed88966a698afb
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-06-01 22:24:43 +0300

    implement streaming orders and order status updates - Change fetchOrders to return Stream - Add updateOrder implementation - Handle errors properly

--- lib/features/orders/data/repos/orders_repo_impl.dart
+++ lib/features/orders/data/repos/orders_repo_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:fruits_hub_admin/core/enums/order_enum.dart';
 import 'package:fruits_hub_admin/core/errors/failures.dart';
 import 'package:fruits_hub_admin/core/services/data_service.dart';
 import 'package:fruits_hub_admin/core/utils/constants/backend_end_points.dart';
@@ -11,14 +12,30 @@ class OrdersRepoImpl implements OrdersRepo {
 
   OrdersRepoImpl({required this.databaseService});
   @override
-  Future<Either<Failure, List<OrderEntity>>> fetchOrders() async {
+  Stream<Either<Failure, List<OrderEntity>>> fetchOrders() async* {
     try {
-      final data =
-          await databaseService.getData(path: BackendEndPoints.getOrders);
-      List<OrderEntity> orders = (data as List<dynamic>)
-          .map<OrderEntity>((e) => OrderModel.fromJson(e).toEntity())
-          .toList();
-      return Right(orders);
+      await for (var data in databaseService.streamData(
+        path: BackendEndPoints.getOrders,
+      )) {
+        List<OrderEntity> orders = (data as List<dynamic>)
+            .map<OrderEntity>((e) => OrderModel.fromJson(e).toEntity())
+            .toList();
+        yield Right(orders);
+      }
+    } catch (e) {
+      yield Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> updateOrder(
+      {required OrderStatusEnum status, required String orderId}) async {
+    try {
+      await databaseService.updateData(
+          path: BackendEndPoints.updateOrder,
+          docuementId: orderId,
+          data: {'status': status.name});
+      return const Right(null);
     } catch (e) {
       return Left(ServerFailure(e.toString()));
     }
