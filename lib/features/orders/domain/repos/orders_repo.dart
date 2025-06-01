7ce0be9f55fc622d4056d9503794a51c275d1a78
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-06-01 22:25:09 +0300

    update interface with streaming and update capabilities - Change fetchOrders to return Stream - Add updateOrder method signature

--- lib/features/orders/domain/repos/orders_repo.dart
+++ lib/features/orders/domain/repos/orders_repo.dart
@@ -1,7 +1,11 @@
 import 'package:dartz/dartz.dart';
+import 'package:fruits_hub_admin/core/enums/order_enum.dart';
 import 'package:fruits_hub_admin/core/errors/failures.dart';
 import 'package:fruits_hub_admin/features/orders/domain/entities/order_entity.dart';
 
 abstract class OrdersRepo {
-  Future<Either<Failure, List<OrderEntity>>> fetchOrders();
+  Stream<Either<Failure, List<OrderEntity>>> fetchOrders();
+
+  Future<Either<Failure, void>> updateOrder(
+      {required OrderStatusEnum status, required String orderId});
 }
