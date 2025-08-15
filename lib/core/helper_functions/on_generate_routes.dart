462b38138dca93c5b72fecb0d327a8332d7c4066
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-15 22:55:35 +0300

    feat: add NotificationView route to onGenerateRoute

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_admin/features/add_product/presentation/views/add_product_view.dart';
 import 'package:fruits_hub_admin/features/dashboard/views/dashboard_view.dart';
+import 'package:fruits_hub_admin/features/notification/presentation/views/notification_view.dart';
 import 'package:fruits_hub_admin/features/orders/presentation/views/orders_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
@@ -16,6 +17,10 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
     // Add Product Screen
     case AddProductView.routeName:
       return MaterialPageRoute(builder: (context) => const AddProductView());
+
+    // Notification Screen
+    case NotificationView.routeName:
+      return MaterialPageRoute(builder: (context) => const NotificationView());
 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
