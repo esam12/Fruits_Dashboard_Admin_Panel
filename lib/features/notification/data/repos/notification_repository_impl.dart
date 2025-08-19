262548a4a6331d526eb82070ccc80df9d25a6d5e
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-19 23:26:23 +0300

    Implement Firebase Functions call to send notifications to users with FCM tokens
    Register FirebaseFunctionsPlugin in macOS GeneratedPluginRegistrant
    
    Update Firebase dependencies to latest versions and add cloud_functions package

--- lib/features/notification/data/repos/notification_repository_impl.dart
+++ lib/features/notification/data/repos/notification_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:fruits_hub_admin/core/services/data_service.dart';
 import 'package:fruits_hub_admin/core/utils/constants/backend_end_points.dart';
 import 'package:fruits_hub_admin/features/notification/data/model/notification_model.dart';
@@ -24,5 +25,24 @@ class NotificationRepositoryImpl implements NotificationRepository {
       docuementId: notification.id,
       data: model.toMap(),
     );
+    final users = await databaseService.getData(path: BackendEndPoints.users);
+
+    final callable =
+        FirebaseFunctions.instance.httpsCallable('sendNotification');
+
+    for (var user in users) {
+      final token = user["fcmToken"];
+      print("Sending notification to token: $token");
+      print("Title: ${notification.title}, Body: ${notification.message}");
+
+      // final userId = user["id"]; // إذا كنت مسجل id مع البيانات
+      if (token != null) {
+        await callable.call({
+          "title": notification.title,
+          "body": notification.message,
+          "token": token,
+        });
+      }
+    }
   }
 }
